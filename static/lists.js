2ba2e6b52f0604f5af181e023f3aeaf5230867a5
camdentadhg <camdent@gmail.com>
2024-04-18 09:53:42 -0500

    working on sorting plugin

--- static/lists.js
+++ static/lists.js
@@ -1,9 +1,24 @@
-$userBookList = $('.user-book-list')
+$userBookList = $('.user-book-list');
+$table = $('.book-list-table');
 
 //on page load, display contents of list
 $(document).ready(function(){
     console.log('event handler started')
     userBooksOnStart();
+    //table sorting and searching plugin
+    $table.fancyTable({
+        sortColumn: 1,
+        sortOrder: 'asc',
+        sortable: true,
+        pagination: true, 
+        paginationClass: 'btn btn-primary',
+        perPage: 25,
+        inputStyle: '',
+        inputPlaceholder: "Search...",
+        globalSearch: true,
+        globalSearchExcludeColumns: [0],
+        exactMatch: "auto"
+    })
 });
 
 //initial function to show book list on site load
@@ -36,3 +51,6 @@ function displayUserBooks(array){
         $userBookList.append($bookTr);
     }
 }
+
+
+
