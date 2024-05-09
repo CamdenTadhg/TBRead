b84954ab0a475086344071e35793fe7fc058f5d3
camdentadhg <camdent@gmail.com>
2024-05-09 13:34:22 -0500

    posting functionality 24

--- app.py
+++ app.py
@@ -796,6 +796,11 @@ def schedule_posting_days():
         flash ('Please log in', 'danger')
         return redirect('/') 
     
+    state = session.get('state')
+    if not state:
+        flash('OAuth state mismatch', 'danger')
+        return redirect('/')
+    
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=['https://www.googleapis.com/auth/calendar.app.created'], state=state)
     flow.redirect_uri = url_for('create_calendar', _external=True)
     authorization_response = request.url
